7176347d69a43edc0ba40d7930af79d146fd6593
ghulam mohi ud din <GHULAM-1>
2024-07-02 02:03:25 +0500

    socket established

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
 const app = express();
-const port = 4000;
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000", // Adjust this if your React app is running on a different port
+    methods: ["GET", "POST"],
+  },
+});
+
+const port = 3001;
+
+io.on("connection", (socket) => {
+  console.log(`New client connected with id: ${socket.id}`);
+
+  socket.on("messageSent", (message) => {
+    console.log(message);
+    socket.emit("messageReceived", socket.id);
+  });
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
